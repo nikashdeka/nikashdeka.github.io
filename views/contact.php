a882dad986b937496f970e75d0699d8e8275e71b
nikashdeka <nikash.deka@gmail.com>
2022-10-17 19:16:22 +0800

    Added: contact.php file.

---
+++ views/contact.php
@@ -0,0 +1,288 @@
+<?php 
+  if($_POST['message']){
+    mail('nikashdeka.developer@gmail.com', 'Hi, want to connect regarding - ',
+
+    $_POST['Write your message here']. 'From: nikashdeka.developer@gmail.com');
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Nikash Deka Portfolio Page</title>
+    <!--Import Google Icon Font-->
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <!-- Import Fontawesome -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+      integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+      crossorigin="anonymous"
+    />
+    <!-- Compiled and minified CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/my_resume_style.css" />
+    <link rel="stylesheet" href="/assets/css/constant_style.css" />
+    <link rel="stylesheet" href="/assets/css/contact.css" />
+    <!-- Import jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </head>
+
+  <body class="white-text" style="background: #131415">
+    <header>
+      <!-- NAVBAR FIXED -->
+      <div class="navbar-fixed">
+        <nav class="z-depth-0">
+          <div class="nav-wrapper container">
+            <a href="/index.html" class="brand-logo light-blue-text"
+              ><strong>Nikash Deka</strong></a
+            >
+            <a
+              href="#"
+              data-target="mobile-menu"
+              class="sidenav-trigger white-text"
+              ><i class="material-icons">menu</i></a
+            >
+            <ul class="right hide-on-med-and-down">
+              <li>
+                <a class="white-text" href="/index.html">Home</a>
+              </li>
+              <li>
+                <a class="white-text" href="/views/projects.html">Projects</a>
+              </li>
+              <li>
+                <a class="white-text" href="/views/about_me.html">About Me</a>
+              </li>
+              <li>
+                <a class="btn-small light-blue" href="/views/contact.html"
+                  >Let's Connect</a
+                >
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- SIDENAV -->
+      <ul class="sidenav" id="mobile-menu">
+        <a href="/index.html" class="brand-logo">
+          <h5 class="pl-2">
+            <strong>Nikash Deka</strong>
+            <h6 class="pl-2">Profile page</h6>
+          </h5>
+        </a>
+        <li><a class="white-text" href="/index.html">Home</a></li>
+        <li>
+          <a class="white-text" href="/views/projects.html">Projects</a>
+        </li>
+        <li>
+          <a class="white-text" href="/views/about_me.html">About Me</a>
+        </li>
+        <li>
+          <a
+            class="btn-small light-blue"
+            class="btn-small"
+            href="/views/contact.html"
+            >Let's Connect</a
+          >
+        </li>
+      </ul>
+      <!-- Nav Section ENDS -->
+    </header>
+
+    <main>
+      <!-- Heading Caption STARTS -->
+      <div class="heading-caption container center-align">
+        <h4 class="center-align">
+          Let's Connect
+          <i class="material-icons medium light-green-text mt-">hdr_strong</i>
+        </h4>
+        <h6 class="light-blue-text mb-3">
+          We Are Here To Help You <br />
+          Thanks for connecting with us.
+        </h6>
+      </div>
+      <!-- Heading Caption ENDS -->
+      <!-- TAB SECTION STARTS -->
+      <!-- <div class="container">
+        <div class="row">
+          <div class="col s12">
+            <ul class="tabs">
+              <li class="tab col s3">
+                <a class="active" href="http://">
+                  <span>WRITE</span>
+                  <span>TO US</span>
+                </a>
+              </li>
+              <li class="tab col s3">
+                <a href="http://">
+                  <span>TALK TO</span>
+                  <span>US</span>
+                </a>
+              </li>
+              <li class="tab col s3">
+                <a href="http://">
+                  <span>JOIN</span>
+                  <span>US</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="container">
+          <div id="writetous" class="col s12">Write To Us</div>
+        </div>
+      </div> -->
+      <!-- TAB SECTION ENDS -->
+
+      <!-- Form STARTS -->
+      <div action="" class="form-container container">
+        <div class="row">
+          <form method="post" action="contactform.php" class="col s12">
+            <div class="row">
+              <!-- FIRST NAME -->
+              <div class="input-field col s6">
+                <input id="first_name" type="text" class="validate" />
+                <label for="first_name">First Name</label>
+              </div>
+              <!-- LAST NAME -->
+              <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" />
+                <label for="last_name">Last Name</label>
+              </div>
+            </div>
+            <!-- MOBILE -->
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="tel" type="tel" class="validate" data-length="10" />
+                <label for="tel">Mobile</label>
+              </div>
+            </div>
+            <!-- EMAIL -->
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" />
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea
+                  name="message"
+                  id="textarea1"
+                  class="materialize-textarea"
+                  data-length="250"
+                ></textarea>
+                <label for="textarea1"
+                  >Write your communication, feedback, or requests here</label
+                >
+              </div>
+            </div>
+            <button
+              class="btn-small waves-effect waves-light light-blue mb-2 mt-1"
+              type="submit"
+              name="action"
+            >
+              Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </form>
+        </div>
+      </div>
+      <!-- Form ENDS -->
+    </main>
+
+    <!-- Footer STARTS -->
+    <footer class="page-footer light-blue">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">
+              Beautiful Minds creates beautiful products.
+            </h5>
+
+            <p class="grey-text text-lighten-4">
+              Let's create something beautiful together!
+            </p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+              <li>
+                <a class="grey-text text-lighten-3" href="/index.html">Home</a>
+              </li>
+              <li>
+                <a class="grey-text text-lighten-3" href="/views/projects.html"
+                  >Projects</a
+                >
+              </li>
+
+              <li>
+                <a
+                  class="grey-text text-lighten-3"
+                  href="/views/my_biodata.html"
+                  >Biodata</a
+                >
+              </li>
+              <li>
+                <a class="grey-text text-lighten-3" href="/views/about_me.html"
+                  >About Me</a
+                >
+              </li>
+              <li>
+                <a
+                  class="btn-small mt-1 light-blue darken-3"
+                  href="/views/certifications.html"
+                  >Credentials</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+          © 2020 Nikash Deka. All Rights Reserved
+          <a id="to-top-btn" class="grey-text text-lighten-4 right" href="#"
+            >To Top</a
+          >
+        </div>
+      </div>
+    </footer>
+    <!-- Footer ENDS -->
+
+    <!--  -->
+    <script src="/assets/js/scroll_to_top.js"></script>
+    <script>
+      $(document).ready(function () {
+        // for the section TABS
+        $(".tabs").tabs();
+        // for for data-length
+        $("input#tel, textarea#textarea1").characterCounter();
+        $(".carousel").carousel({
+          indicators: true,
+        });
+        $(".tabs").tabs();
+        $(".slider").slider({
+          height: 300,
+        });
+        $(".sidenav").sidenav();
+        $(".carousel.carousel-slider").carousel({
+          fullWidth: true,
+          indicators: true,
+        });
+      });
+    </script>
+  </body>
+</html>
